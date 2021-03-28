79cb0a519cce965a5c1fd6c3bb2b61870036dae8
Daria <yudina.daria.antonovna@gmail.com>
2021-03-28 14:33:25 +0500

    хочу спать

--- pkg/bank/card/card.go
+++ pkg/bank/card/card.go
@@ -9,11 +9,14 @@ import (
 // карты с отрицательным балансом и  заблокированные карты игнорируются 
 func PaymentSources(cards []types.Card) [] types.PaymentSource {
 	var paymentSourses [] types.PaymentSource
+	i :=0 
       for _, card := range cards {
 	  if  card.Balance<= 0 && !card.Active{
 		continue
  
 	  }
+	  paymentSourses = make([]types.PaymentSource, 45)
+    i++
  }
 
   return paymentSourses 
