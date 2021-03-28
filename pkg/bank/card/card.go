0cfb8dc92a9bf2f5263c3ee6dfc086e6436359ab
Daria <yudina.daria.antonovna@gmail.com>
2021-03-28 17:29:49 +0500

    код, что как и я держится на одной вере

--- pkg/bank/card/card.go
+++ pkg/bank/card/card.go
@@ -8,14 +8,14 @@ import (
 // PaymentSources предоставляет выбор карт для оплаты 
 // карты с отрицательным балансом и  заблокированные карты игнорируются 
 func PaymentSources(cards []types.Card) [] types.PaymentSource {
-	var payments[] types.PaymentSource
+	var payments []types.PaymentSource
       for _, card := range cards {
 	  if  card.Balance<= 0 && !card.Active{
 		continue
  
-	  }
-   
- }
+	}
+	payments = append(payments,types.PaymentSource{"card","9074 9934 8645 7642",1_000_00})
+ 	}
 
     return payments
 } 
--- pkg/bank/card/exemple_test.go
+++ pkg/bank/card/exemple_test.go
@@ -7,30 +7,32 @@ import (
 
 
 func ExemplePaymentSources() {
-	payments := []types.Card{
+	fmt.Println(PaymentSources( []types.Card{
+		{
+			Balance: 1_000_00,
+			PAN: "5555 6666 7777 8878",
+			Active: true,
+			Currency: "TJS",
+ 		},
+	}))
+	
+fmt.Println(PaymentSources( []types.Card{
 	{
 		Balance: 1_000_00,
-		PAN: "5555 6666 7777 8878",
+		PAN: "5678 9876 8765 8798",
 		Active: false,
 		Currency: "TJS",
- 	},
-	{
-		Balance: 3_000_00,
-		PAN: "5678 9876 8765 8798",
-		Active: true,
-		Currency: "TJS",
 	},
-	{
-		Balance: -1_000_00,
-		PAN: "9074 9934 8645 7642",
-		Active: true,
-		Currency: "TJS",
-	},
-  }
+}))
 
-  payment := PaymentSources(payments)
-    fmt.Println(payment)
+	fmt.Println(PaymentSources( []types.Card{
+		{
+			Balance: -1_000_00,
+			PAN: "9074 9934 8645 7642",
+			Active: true,
+			Currency: "TJS",
+		},
+	}))
    //Output: 
-   //5678 9876 8765 8798"
-   
+   //5555 6666 7777 8878(100000 TJS)
 }
